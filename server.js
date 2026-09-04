7d367958edfe54e6fb9cbd74773218f84e1c3cb5
DmitryKishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2026-09-04 21:19:33 +0300

    подключил mongoose

---
+++ server.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+
+const DB_HOST =
+  "mongodb+srv://Dmitry:fHSSUdFJof8LOAce@cluster0.appzis8.mongodb.net/books_reader";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Server is running. Use our API on port: 3000");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1); // Эта команда закрывает запущенные процессы
+  });
