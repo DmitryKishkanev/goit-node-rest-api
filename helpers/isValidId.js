a5791e6dc45eda9e938e2ef4bbf4ad91196dc59d
DmitryKishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2026-09-14 12:02:16 +0300

    исправил мидлвару

--- helpers/isValidId.js
+++ helpers/isValidId.js
@@ -1,10 +1,10 @@
 import { isValidObjectId } from "mongoose";
-import HttpError from "./index.js";
+import helpers from "./index.js";
 
 export const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    next(HttpError(400, `${id} is not valid id`));
+    next(helpers.HttpError(400, `${id} is not valid id`));
   }
   next();
 };
