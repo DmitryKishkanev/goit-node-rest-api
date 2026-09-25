a2d019de8ffdd1c35d9a04a0a42a5f5974422c8e
DmitryKishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2026-09-25 19:52:01 +0300

    исправил импорт HttpError

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,6 +2,13 @@ import userModel from "../models/user.js";
 import helpers from "../helpers/index.js";
 
 const register = async (req, res) => {
+  const { email } = req.body;
+  const user = await userModel.User.findOne({ email });
+
+  if (user) {
+    throw helpers.HttpError(409, "Email already in use");
+  }
+
   const newUser = await userModel.User.create(req.body);
 
   res.status(201).json({
--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,13 +1,8 @@
-const handleMongooseError = (error, data, next) => {
-  error.status = 400;
+export const handleMongooseError = (error, data, next) => {
+  const { name, code } = error;
+  const status = name === "MongoServerError" && code === 11000 ? 409 : 400;
+  error.status = status;
   next();
 };
 
 export default handleMongooseError;
-
-// export const handleMongooseError = (error, data, next) => {
-//   const { name, code } = error;
-//   const status = name === "MongoServerError" && code === 11000 ? 409 : 400;
-//   error.status = status;
-//   next();
-// };
